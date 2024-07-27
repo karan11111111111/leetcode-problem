d9ba511467783bf660897cc2af7520be390f95e2
Karan <144606876+karan11111111111@users.noreply.github.com>
2024-07-27 17:00:15 +0530

    Time: 3 ms (60.01%) | Memory: 54.2 MB (15.48%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -2,18 +2,17 @@ class Solution {
     public int findJudge(int n, int[][] trust) {
         if (n == 1) return 1; // If there's only one person, they are the judge by definition
 
-        int[] trustCount = new int[n + 1];
-        int[] trustedBy = new int[n + 1];
+        int[] trustBalance = new int[n + 1];
 
         for (int[] t : trust) {
             int a = t[0];
             int b = t[1];
-            trustCount[b]++;
-            trustedBy[a]++;
+            trustBalance[a]--;
+            trustBalance[b]++;
         }
 
         for (int i = 1; i <= n; i++) {
-            if (trustCount[i] == n - 1 && trustedBy[i] == 0) {
+            if (trustBalance[i] == n - 1) {
                 return i; // i is the town judge
             }
         }
