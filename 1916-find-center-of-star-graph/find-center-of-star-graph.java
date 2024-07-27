2ebc8d67be8fb5a30caac3006ba05812ce86b118
Karan <144606876+karan11111111111@users.noreply.github.com>
2024-07-27 17:19:06 +0530

    Time: 3 ms (25.66%) | Memory: 67.4 MB (35.21%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.java
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.java
@@ -1,14 +1,21 @@
 class Solution {
     public int findCenter(int[][] edges) {
-        // Take the first two edges
-        int[] edge1 = edges[0];
-        int[] edge2 = edges[1];
-        
-        // The center node is the one that appears in both edges
-        if (edge1[0] == edge2[0] || edge1[0] == edge2[1]) {
-            return edge1[0];
-        } else {
-            return edge1[1];
+        int n = edges.length + 1; // Number of nodes
+        int[] indegree = new int[n + 1];
+
+        // Calculate the indegree of each node
+        for (int[] edge : edges) {
+            indegree[edge[0]]++;
+            indegree[edge[1]]++;
         }
+
+        // The center node will have an indegree of n-1
+        for (int i = 1; i <= n; i++) {
+            if (indegree[i] == n - 1) {
+                return i;
+            }
+        }
+
+        return -1; // Just a fallback, the problem guarantees there is a center
     }
 }
