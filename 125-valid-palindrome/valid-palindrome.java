0076550896ebd5b6240f13abcd2593ac97222eca
Karan <144606876+karan11111111111@users.noreply.github.com>
2024-08-02 12:29:01 +0530

    Time: 2 ms (99.07%) | Memory: 42.8 MB (80.51%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,24 +1,41 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        // Convert the string to lowercase
-        s = s.toLowerCase();
-        
-        // Remove non-alphanumeric characters
-        s = s.replaceAll("[^a-z0-9]", "");
-        
-        // Initialize pointers for comparison
+        var size = s.length();
         int left = 0;
-        int right = s.length() - 1;
+        int right = size - 1;
         
-        // Compare characters from both ends
         while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
-                return false;
+            while (left < right && !isValidChar(s, left)) {
+                left++;
             }
+            
+            while (right > left && !isValidChar(s, right)) {
+                right--;
+            }
+            
+            var leftChar = toLowerCase(s.charAt(left));
+            var rightChar = toLowerCase(s.charAt(right));
+            
+            if (leftChar != rightChar) return false;
             left++;
             right--;
         }
         
         return true;
+    }
+    
+    private boolean isValidChar(String s, int index) {
+        var character = s.charAt(index);
+        
+        return ((character >= '0' && character <= '9') || (character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z'));
+    }
+    
+    private char toLowerCase(char character) {
+        var pos = character - 'A';
+        if (pos >= 0 && pos <= 25) {
+            return (char) (pos + 'a');
+        }
+        
+        return character;
     }
 }
