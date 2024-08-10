861e81edd23261c11613be01907e67060a41b42c
Karan <144606876+karan11111111111@users.noreply.github.com>
2024-08-10 23:40:51 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (16.28%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -17,37 +17,57 @@
 //     }
 // }
 
+// class Solution {
+//     public void sortColors(int[] nums) {
+//         quickSort(nums, 0, nums.length - 1);
+//     }
+
+//     private void quickSort(int[] nums, int low, int high) {
+//         if (low < high) {
+//             int pivotIndex = partition(nums, low, high);
+//             quickSort(nums, low, pivotIndex - 1);  
+//             quickSort(nums, pivotIndex + 1, high); 
+//         }
+//     }
+
+//     private int partition(int[] nums, int low, int high) {
+//         int pivot = nums[high];
+//         int i = low - 1;
+
+//         for (int j = low; j < high; j++) {
+//             if (nums[j] <= pivot) {
+//                 i++;
+//                 swap(nums, i, j);
+//             }
+//         }
+
+//         swap(nums, i + 1, high); 
+//         return i + 1; 
+//     }
+
+//     private void swap(int[] nums, int i, int j) {
+//         int temp = nums[i];
+//         nums[i] = nums[j];
+//         nums[j] = temp;
+//     }
+// }
+
 class Solution {
     public void sortColors(int[] nums) {
-        quickSort(nums, 0, nums.length - 1);
-    }
-
-    private void quickSort(int[] nums, int low, int high) {
-        if (low < high) {
-            int pivotIndex = partition(nums, low, high);
-            quickSort(nums, low, pivotIndex - 1);  // Recursively sort elements before pivot
-            quickSort(nums, pivotIndex + 1, high); // Recursively sort elements after pivot
+        int count_0 = 0 , count_1 = 0 , count_2 = 0;
+        for(int i=0 ; i<nums.length ; i++){
+            if(nums[i] == 0)
+            count_0++;
+            if(nums[i] == 1)
+            count_1++;
+            if(nums[i] == 2)
+            count_2++;
         }
-    }
-
-    private int partition(int[] nums, int low, int high) {
-        int pivot = nums[high]; // Choosing the last element as the pivot
-        int i = low - 1;
-
-        for (int j = low; j < high; j++) {
-            if (nums[j] <= pivot) {
-                i++;
-                swap(nums, i, j);
-            }
-        }
-
-        swap(nums, i + 1, high); // Place the pivot in its correct position
-        return i + 1; // Return the pivot index
-    }
-
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+        for(int i = 0 ; i<count_0 ; i++)
+        nums[i] = 0;
+         for(int i = count_0 ; i<(count_0+count_1) ; i++)
+        nums[i] = 1;
+         for(int i = count_0+count_1 ; i<(count_0+count_1+count_2) ; i++)
+        nums[i] = 2;
     }
 }
