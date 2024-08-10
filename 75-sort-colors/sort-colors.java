21c92057ba4e607162ba8e3ff8ca385380d47cdd
Karan <144606876+karan11111111111@users.noreply.github.com>
2024-08-10 21:52:13 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (9.40%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,18 +1,53 @@
+// class Solution {
+//     public void sortColors(int[] nums) {
+//         // Arrays.sort(nums);
+
+//     int n = nums.length;
+
+//     for(int i=0; i<n; i++){
+//         for(int j=0; j<n-i-1; j++){
+//            if(nums[j]>nums[j+1]){
+//             int temp = nums[j];
+//             nums[j] = nums[j+1];
+//             nums[j+1] = temp;
+//            }
+//         }
+//     }
+
+//     }
+// }
+
 class Solution {
     public void sortColors(int[] nums) {
-        // Arrays.sort(nums);
+        quickSort(nums, 0, nums.length - 1);
+    }
 
-    int n = nums.length;
-
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n-i-1; j++){
-           if(nums[j]>nums[j+1]){
-            int temp = nums[j];
-            nums[j] = nums[j+1];
-            nums[j+1] = temp;
-           }
+    private void quickSort(int[] nums, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(nums, low, high);
+            quickSort(nums, low, pivotIndex - 1);  // Recursively sort elements before pivot
+            quickSort(nums, pivotIndex + 1, high); // Recursively sort elements after pivot
         }
     }
 
+    private int partition(int[] nums, int low, int high) {
+        int pivot = nums[high]; // Choosing the last element as the pivot
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (nums[j] <= pivot) {
+                i++;
+                swap(nums, i, j);
+            }
+        }
+
+        swap(nums, i + 1, high); // Place the pivot in its correct position
+        return i + 1; // Return the pivot index
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
