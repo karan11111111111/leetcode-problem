73e3086e1d009322fc7d3474a007208fa2b2023b
Karan <144606876+karan11111111111@users.noreply.github.com>
2024-08-12 20:20:32 +0530

    Time: 1 ms (99.86%) | Memory: 45.9 MB (57.84%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,3 +1,16 @@
+// class Solution {
+//     public int singleNumber(int[] nums) {
+
+//         //a^a = 0;
+//         //a^0 = a;
+//         int xor =0;
+//         for(int i=0; i<nums.length; i++){
+//             xor = xor^nums[i];
+//         }
+//         return xor;
+//     }
+// }
+
 class Solution {
     public int singleNumber(int[] nums) {
         int result = 0;
