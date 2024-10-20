6c4b0164c77d2ab3137856aa54ec89b843d31fb1
Karan <144606876+karan11111111111@users.noreply.github.com>
2024-10-21 01:16:54 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (26.83%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.java
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -18,16 +18,16 @@ class Solution {
 
        ListNode odd = head;
        ListNode even = head.next;
-       ListNode evenHead = even;
+       ListNode evenHead = even;  // We'll need to connect the end of odd list to even list
         
     
         while(even != null && even.next !=null){
-          odd.next = even.next;
-          odd = odd.next;
-          even.next = odd.next;
-          even = even.next;
+          odd.next = even.next; // Link current odd node to the next odd node
+          odd = odd.next; // Move odd pointer
+          even.next = odd.next; // Link current even node to the next even node
+          even = even.next; // Move even pointer
         }
-
+// Connect the end of odd list to the start of the even list
        odd.next = evenHead;
         
         return head;
