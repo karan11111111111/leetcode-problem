cdc326ea74888d73fdc3acdcacb85b294772e878
Karan <144606876+karan11111111111@users.noreply.github.com>
2024-10-22 00:07:12 +0530

    Time: 454 ms (5.28%) | Memory: 45.1 MB (95.61%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        boolean[] dp = new boolean[n];
+        dp[0] = true; // You can always start at the first index
+
+        for (int i = 0; i < n; i++) {
+            if (dp[i]) { // If you can reach index i
+                for (int j = i + 1; j <= Math.min(i + nums[i], n - 1); j++) {
+                    dp[j] = true; // You can reach index j from index i
+                }
+            }
+        }
+
+        return dp[n - 1]; // Check if you can reach the last index
+    }
+}
