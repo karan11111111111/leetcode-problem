15975b2f0adee1436b40983af2e33dbbf36ae305
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-01-03 21:01:33 +0530

    Time: 8 ms (70.88%) | Memory: 60.5 MB (30.60%) - LeetSync

---
+++ 221-maximal-square/maximal-square.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int maxSide = 0;
+
+        // Create a DP array to store the maximum square side length ending at each cell
+        int[][] dp = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == '1') {
+                    if (i == 0 || j == 0) {
+                        // If we are in the first row or column, the square side is 1
+                        dp[i][j] = 1;
+                    } else {
+                        // Calculate the minimum of the three neighbors plus 1
+                        dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    }
+                    maxSide = Math.max(maxSide, dp[i][j]);
+                }
+            }
+        }
+
+        // The area of the largest square
+        return maxSide * maxSide;
+    }
+}
