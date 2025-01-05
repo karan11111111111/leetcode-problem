9dceedb9087f6703185f0665ffdf967a4e79cde0
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-01-06 01:12:39 +0530

    Time: 4 ms (52.26%) | Memory: 53.2 MB (42.90%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,20 +1,30 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n = nums.length;
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<n; i++){
-            if(map.containsKey(nums[i])){
-                map.put(nums[i], map.get(nums[i])+1);
-            }else{
-                map.put(nums[i], 1);
-            }
+        // int n = nums.length;
+        // Map<Integer, Integer> map = new HashMap<>();
+        // for(int i=0; i<n; i++){
+        //     if(map.containsKey(nums[i])){
+        //         map.put(nums[i], map.get(nums[i])+1);
+        //     }else{
+        //         map.put(nums[i], 1);
+        //     }
+        // }
+        // for (Map.Entry<Integer, Integer>entry:map.entrySet()){
+        //     if(entry.getValue() > n/2){
+        //         return entry.getKey();
+        //     }
+        // }
+        // return -1;
+
+
+        if (nums.length == 0) {
+            return -1; // Return an invalid value since there is no majority element.
         }
-        for (Map.Entry<Integer, Integer>entry:map.entrySet()){
-            if(entry.getValue() > n/2){
-                return entry.getKey();
-            }
-        }
-        return -1;
+
+        Arrays.sort(nums);
+        return nums[nums.length / 2]; // The majority element is guaranteed to be at the middle after sorting.
+    
     }
+
 }
 
