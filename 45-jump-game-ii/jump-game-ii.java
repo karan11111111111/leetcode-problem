21341e88f387b64b40cb34e9fe0b88fbc31c7e4e
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-01-06 19:31:35 +0530

    Time: 1 ms (99.25%) | Memory: 44.8 MB (87.16%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,20 +1,44 @@
+// class Solution {
+//     public int jump(int[] nums) {
+//         int n = nums.length;
+//         int [] dp = new int[n];
+        
+        
+//         for (int i = 1; i < n; i++) {
+//           dp[i] = Integer.MAX_VALUE;
+//         }
+
+
+//        for(int i=0; i<n; i++){
+//         for(int j =i+1; j<= Math.min(i+nums[i], n-1);j++){
+//             dp[j] = Math.min(dp[j] , dp[i]+1);
+//         }
+//        }
+//         return dp[n - 1]; 
+        
+//     }
+// }
+
 class Solution {
     public int jump(int[] nums) {
         int n = nums.length;
-        int [] dp = new int[n];
-        
-        
-        for (int i = 1; i < n; i++) {
-          dp[i] = Integer.MAX_VALUE;
+        int jumps = 0; // Number of jumps
+        int far = 0; // Farthest point that can be reached
+        int currentEnd = 0; // Current jump range
+
+        for (int i = 0; i < n - 1; i++) { 
+            far = Math.max(far, i + nums[i]); 
+
+            if (i == currentEnd) { 
+                jumps++;
+                currentEnd = far; 
+
+                if (currentEnd >= n - 1) { 
+                    break;
+                }
+            }
         }
 
-
-       for(int i=0; i<n; i++){
-        for(int j =i+1; j<= Math.min(i+nums[i], n-1);j++){
-            dp[j] = Math.min(dp[j] , dp[i]+1);
-        }
-       }
-        return dp[n - 1]; 
-        
+        return jumps;
     }
 }
