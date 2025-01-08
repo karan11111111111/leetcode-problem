74c0812d981567a7166e2c9c2f3c2e6d15554936
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-01-08 12:21:09 +0530

    Time: 2 ms (65.62%) | Memory: 41.3 MB (77.41%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,35 +1,56 @@
-class Solution {
-    public boolean isSubsequence(String s, String t) {
+// class Solution {
+//     public boolean isSubsequence(String s, String t) {
         
-            int n = s.length();
-            int m = t.length();
-        if(n==0){
-            return true;
-        }
-        if(m==0){
-            return false;
-        }
+//             int n = s.length();
+//             int m = t.length();
+//         if(n==0){
+//             return true;
+//         }
+//         if(m==0){
+//             return false;
+//         }
 
-            boolean dp[][] = new boolean[n+1][m+1];
+//             boolean dp[][] = new boolean[n+1][m+1];
 
           
-           for(int j=0; j<=m; j++){
-            dp[0][j] = true;
-           }
+//            for(int j=0; j<=m; j++){
+//             dp[0][j] = true;
+//            }
 
-           for(int i=1; i<=n; i++){
-              for(int j=1; j<=m; j++){
-                if(s.charAt(i-1) == t.charAt(j-1)){
-                    dp[i][j] = dp [i-1][j-1];
-                }else{
-                    dp[i][j] = dp[i][j-1];
-                }
-              }
-           }
+//            for(int i=1; i<=n; i++){
+//               for(int j=1; j<=m; j++){
+//                 if(s.charAt(i-1) == t.charAt(j-1)){
+//                     dp[i][j] = dp [i-1][j-1];
+//                 }else{
+//                     dp[i][j] = dp[i][j-1];
+//                 }
+//               }
+//            }
            
         
-        return dp[n][m];
+//         return dp[n][m];
 
 
+//     }
+// }
+
+
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() == 0) {
+            return true; 
+        }
+        
+        int i = 0; 
+        int j = 0; 
+
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++; 
+            }
+            j++; 
+        }
+
+        return i == s.length(); 
     }
 }
