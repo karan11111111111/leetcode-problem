31fac7154168d2b48557fb9f95de59accfc34e6f
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-01-17 11:45:11 +0530

    Time: 13 ms (33.41%) | Memory: 44.6 MB (43.38%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        HashMap<Character,Integer> sMap = new HashMap<>();
+        HashMap<Character,Integer> tMap = new HashMap<>();
+
+        for(char c: s.toCharArray() ){
+            sMap.put(c,sMap.getOrDefault(c,0)+1);
+        }
+        for(char c: t.toCharArray() ){
+            tMap.put(c,tMap.getOrDefault(c,0)+1);
+        }
+
+        if( sMap.equals(tMap)){
+            return true;
+        }
+
+        return false;
+    }
+}
