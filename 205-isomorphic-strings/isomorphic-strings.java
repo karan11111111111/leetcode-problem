dacf4f43f9ed2af0998b4d8ec56e0cf5259dd9aa
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-01-17 11:25:16 +0530

    Time: 17 ms (37.57%) | Memory: 42.7 MB (23.83%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,26 +1,68 @@
+// class Solution {
+//     public boolean isIsomorphic(String s, String t) {
+//         if (s.length() != t.length()) return false;
+
+    
+//         int[] mapST = new int[256];
+//         int[] mapTS = new int[256];
+
+    
+//         for (int i = 0; i < s.length(); i++) {
+//             char charS = s.charAt(i);
+//             char charT = t.charAt(i);
+
+           
+//             if (mapST[charS] == 0 && mapTS[charT] == 0) {
+               
+//                 mapST[charS] = charT;
+//                 mapTS[charT] = charS;
+//             } else if (mapST[charS] != charT || mapTS[charT] != charS) {
+//                 return false; 
+//             }
+//         }
+
+//         return true;
+//     }
+// }
+
+import java.util.HashMap;
+
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if (s.length() != t.length()) return false;
+        
+        if (s.length() != t.length()) {
+            return false;
+        }
 
-        // Maps to store the mapping of characters from s to t and vice versa
-        int[] mapST = new int[256];
-        int[] mapTS = new int[256];
+     
+        HashMap<Character, Character> sMap = new HashMap<>();
+        HashMap<Character, Character> tMap = new HashMap<>();
 
-        // Iterate through the strings
+        
         for (int i = 0; i < s.length(); i++) {
             char charS = s.charAt(i);
             char charT = t.charAt(i);
 
-            // Check if the mapping is consistent
-            if (mapST[charS] == 0 && mapTS[charT] == 0) {
-                // Create a new mapping
-                mapST[charS] = charT;
-                mapTS[charT] = charS;
-            } else if (mapST[charS] != charT || mapTS[charT] != charS) {
-                return false; // Mapping is inconsistent
+           
+            if (sMap.containsKey(charS)) {
+                if (sMap.get(charS) != charT) {
+                    return false; 
+                }
+            } else {
+                sMap.put(charS, charT);
+            }
+
+            
+            if (tMap.containsKey(charT)) {
+                if (tMap.get(charT) != charS) {
+                    return false;  
+                }
+            } else {
+                tMap.put(charT, charS);
             }
         }
 
+       
         return true;
     }
 }
