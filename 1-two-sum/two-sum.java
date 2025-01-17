35e91c0d6ff918a252ba7ddfa8c7817e36613b44
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-01-17 12:30:10 +0530

    Time: 3 ms (65.29%) | Memory: 45 MB (41.60%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,23 +1,61 @@
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//        int n = nums.length;
+
+//        int b[] =new  int[2];
+//        for(int i=0; i<n; i++){
+//         for(int j=i+1; j<n; j++){
+//             if(nums[i]+nums[j]==target){
+//               b[0] = i;
+//               b[1] = j;  
+//               return b;
+//             }
+//         }
+//        }
+
+//     return null;
+//     }
+// }
+
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//          for(int i = 1;i<nums.length;i++)
+//          {
+//             for(int j=i;j<nums.length;j++)
+//             {
+//                 if(nums[j]+nums[j-i]==target)
+//                 {
+//                     return new int[] {j,j-i};
+//                 }
+//             }
+//          }
+//          return null;
+//     }
+// }
+
+
 import java.util.HashMap;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
+        // Create a HashMap to store the number and its index
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        
+        // Iterate through the array
         for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];  
+            int complement = target - nums[i];  // Find the complement
 
-    
+            // Check if the complement is already in the map
             if (map.containsKey(complement)) {
-                 return new int[] { map.get(complement), i };
+                // Return the indices of the complement and the current number
+                return new int[] { map.get(complement), i };
             }
 
-              map.put(nums[i], i);
+            // Add the current number and its index to the map
+            map.put(nums[i], i);
         }
 
-        
+        // Return null if no solution is found
         return null;
     }
 }
