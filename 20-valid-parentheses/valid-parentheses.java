414e7938609e09612c883fb9f2460cd12ead85e5
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-01-19 11:28:16 +0530

    Time: 2 ms (97.68%) | Memory: 41.8 MB (47.38%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -2,12 +2,12 @@ import java.util.Stack;
 
 class Solution {
     public boolean isValid(String s) {
-        // Stack to keep track of opening brackets
+       
         Stack<Character> stack = new Stack<>();
 
-        // Iterate through the string
+        
         for (char c : s.toCharArray()) {
-            // Push the corresponding closing bracket onto the stack
+    
             if (c == '(') {
                 stack.push(')');
             } else if (c == '{') {
@@ -15,7 +15,7 @@ class Solution {
             } else if (c == '[') {
                 stack.push(']');
             } 
-            // If a closing bracket doesn't match the top of the stack, return false
+       
             else if (stack.isEmpty() || stack.pop() != c) {
                 return false;
             }
