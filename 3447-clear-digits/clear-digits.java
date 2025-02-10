68b6f264636461269cc65daedb7cabab12621667
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-02-10 18:51:38 +0530

    Time: 1 ms (100.00%) | Memory: 42.8 MB (45.64%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String clearDigits(String s) {
+        StringBuilder om = new StringBuilder();
+        int i;
+        for(i=0; i<s.length(); i++)
+        {
+            if(s.charAt(i)>='0'&&s.charAt(i)<='9')
+            {
+                om.deleteCharAt(om.length()-1);
+            }
+            else
+            {
+                om.append(s.charAt(i));
+            }
+        }
+        return om.toString();
+    }
+}
