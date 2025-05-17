d886a4e1ee9fd506f785ed14cdea82bf4b486fc3
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-05-17 16:08:29 +0530

    Time: 2 ms (74.94%) | Memory: 61.6 MB (74.00%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,33 +1,77 @@
 class Solution {
     public int maxProfit(int[] prices) {
 
-        if(prices == null || prices.length ==0){
-            return 0;
-        }
+        // brute force
+
+        // int maxprofit = 0;
+
+        // if (prices.length <= 1 || prices == null) {
+        //     return 0;
+        // }
+
+        // for (int i = 0; i < prices.length - 1; i++) {
+        //     for (int j = i + 1; j < prices.length; j++) {
+
+        //         int profit = prices[j] - prices[i];
+
+        //         if (profit > maxprofit) {
+        //             maxprofit = profit;
+        //         }
+
+        //     }
+        // }
+
+        // return maxprofit;
+
 
         int min = Integer.MAX_VALUE;
-        int max = 0;
+        
 
-        for(int i=0; i<prices.length; ++i){
-         if(prices[i]< min){
-            min = prices[i]; 
-         }
+        int profit =0;
+        int maxprofit =0;
 
-         int profit = prices[i] - min;
+        for(int i=0; i<prices.length; i++){
+            if(prices[i] <min){
+                min = prices[i];
+            }
 
-         if(profit>max){
-            max = profit;
-         }
+            profit = prices[i] - min;
+
+            if(profit >maxprofit){
+                maxprofit = profit;
+            }
         }
 
-        return max;
-        
+     return maxprofit;
+
     }
 }
 
-
 // class Solution {
+
 //         public int maxProfit(int[] prices) {
+
+// if(prices == null || prices.length ==0){
+//     return 0;
+// }
+
+// int min = Integer.MAX_VALUE;
+// int max = 0;
+
+// for(int i=0; i<prices.length; ++i){
+//  if(prices[i]< min){
+//     min = prices[i]; 
+//  }
+
+//  int profit = prices[i] - min;
+
+//  if(profit>max){
+//     max = profit;
+//  }
+// }
+
+// return max;
+
 //             int n = prices.length;
 //         if(n > 100 && prices[0] == 397) return 9995;
 //         if(n > 100 && prices[0] == 10000) return 3;
