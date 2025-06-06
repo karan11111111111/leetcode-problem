ac4dd260fb72dbc37e45a53ce4f88c7b07d91efd
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-06-06 08:16:15 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (52.09%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -17,3 +17,60 @@ class Solution {
 
 }
 
+// class Solution {
+//     public boolean check(int[] nums) {
+
+//         int n = nums.length;
+
+//         int [] arr =new int[n];
+
+//         int k =1;
+//         for(int i=0; i<n; i++){
+//             arr[i] = nums[i];
+//         }
+
+//         for(int i=1; i<n; i++){
+//             if(nums[i] >=nums[i-1] ){
+//                 k++;
+//                 break;
+//             }
+//         }
+
+//         Arrays.sort(nums);
+
+//         rotate(nums, k);
+//        for(int i=0; i<n; i++){
+//         if(nums[i] != arr[i]){
+//             return true;
+//         }
+
+//        }
+
+//        return false;
+        
+//     }
+
+//     public static void rotate(int [] arr, int k){
+//         int n = arr.length;
+//         k = k%n;
+
+//         reverse(arr, 0, k-1);
+//         reverse(arr, k, n-1);
+//         reverse(arr, 0, n-1);
+
+//     }
+
+//     public static void reverse(int[] arr, int start, int end){
+
+//         start = 0;
+//         end = arr.length-1;
+
+//         while(start < end){
+//             int temp = arr[start];
+//             arr[start] = arr[end];
+//             arr[end] = temp;
+//             start++;
+//             end--;
+//         }
+//     }
+// }
