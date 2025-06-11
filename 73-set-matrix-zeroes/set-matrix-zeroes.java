161d23dd89502cd4f751859cd77c1f99e5dac4f0
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-06-11 07:35:31 +0530

    Time: 1 ms (72.17%) | Memory: 45.4 MB (91.83%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,36 +1,26 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
       
-        int nRows = matrix.length;
-        int nCols = matrix[0].length;
 
-        boolean[] zeroRows = new boolean[nRows];
-        boolean[] zeroCols = new boolean[nCols];
+        int n = matrix.length;
+        int m = matrix[0].length;
+       int[][] temp = new int[n][m] ;
 
-        // Step 1: Mark the rows and columns that should be zeroed
-        for (int i = 0; i < nRows; i++) {
-            for (int j = 0; j < nCols; j++) {
-                if (matrix[i][j] == 0) {
-                    zeroRows[i] = true;
-                    zeroCols[j] = true;  // Corrected index
-                }
-            }
+       for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            temp[i][j] = matrix[i][j];
         }
+       }
 
-        // Step 2: Zero out marked rows
-        for (int i = 0; i < nRows; i++) {
-            if (zeroRows[i]) {
-                for (int j = 0; j < nCols; j++) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-
-        // Step 3: Zero out marked columns
-        for (int j = 0; j < nCols; j++) {  // Corrected loop to iterate over columns
-            if (zeroCols[j]) {
-                for (int i = 0; i < nRows; i++) {
-                    matrix[i][j] = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(temp[i][j] == 0){
+                    for(int k=0; k<m; k++){
+                        matrix[i][k] =0;
+                    }
+                    for(int k=0; k<n; k++){
+                        matrix[k][j] =0;
+                    }
                 }
             }
         }
