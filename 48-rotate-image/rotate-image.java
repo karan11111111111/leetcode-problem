1cbe3534d2126b9ab30371584a8a24ae4643726e
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-06-11 08:31:39 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (14.34%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,22 +1,51 @@
+// class Solution {
+//     public void rotate(int[][] matrix) {
+//         int n = matrix.length;
+//         int[][] temp = new int[n][n];
+
+//         // Copy values to the rotated positions
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 temp[j][n - 1 - i] = matrix[i][j];
+//             }
+//         }
+
+//         // Copy temp back to original matrix
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 matrix[i][j] = temp[i][j];
+//             }
+//         }
+//     }
+// }
+
 class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
-        int[][] temp = new int[n][n];
 
-        // Copy values to the rotated positions
+        // Step 1: Transpose the matrix (swap matrix[i][j] with matrix[j][i])
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                temp[j][n - 1 - i] = matrix[i][j];
+            for (int j = i + 1; j < n; j++) {
+                // Swap
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
 
-        // Copy temp back to original matrix
+        // Step 2: Reverse each row
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                matrix[i][j] = temp[i][j];
+            int start = 0, end = n - 1;
+            while (start < end) {
+                // Swap
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
+
+                start++;
+                end--;
             }
         }
     }
 }
-
 
