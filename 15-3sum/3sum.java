b096b380692da041c2eea37803e144182c6c1a4f
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-06-12 08:33:14 +0530

    Time: 31 ms (57.77%) | Memory: 51.4 MB (83.32%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -45,3 +45,33 @@ class Solution {
         return list;
     }
 }
+
+
+// Brute Force
+
+
+// class Solution {
+//     public List<List<Integer>> threeSum(int[] nums) {
+        
+//         Set<List<Integer>> result = new HashSet<>(); 
+        
+//         List<Integer> list = new ArrayList<>();
+
+//         int n = nums.length;
+
+//         for(int i=0; i<n-2; i++){
+//             for(int j=i+1; j<n-1; j++){
+//                 for(int k = j+1; k<n; k++){
+//                     if(nums[i]+nums[j]+nums[k] ==0){
+//                         list = Arrays.asList(nums[i], nums[j] , nums[k]);
+//                         Collections.sort(list);
+//                         result.add(list);
+//                     }
+//                 }
+
+//             }
+//         }
+
+//         return new ArrayList<>(result);
+//     }
+// }
