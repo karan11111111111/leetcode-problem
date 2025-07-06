f26c8e5fc9609cae01521be54cb51c785c3bee28
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-07-06 16:12:28 +0530

    Time: 7 ms (71.76%) | Memory: 44.9 MB (27.02%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,20 +1,83 @@
+// class Solution {
+//     public int findTargetSumWays(int[] nums, int target) {
+//         return countWays(nums, 0, target);
+//     }
+
+//     private int countWays(int[] nums, int index, int target) {
+        
+//         if (index == nums.length) {
+//             return (target == 0) ? 1 : 0;
+//         }
+
+        
+//         int add = countWays(nums, index + 1, target - nums[index]);
+
+        
+//         int subtract = countWays(nums, index + 1, target + nums[index]);
+
+//         return add + subtract;
+//     }
+// }
+
+
+// import java.util.*;
+
+// class Solution {
+//     public int findTargetSumWays(int[] nums, int target) {
+//         Map<String, Integer> memo = new HashMap<>();
+//         return countWays(nums, 0, target, memo);
+//     }
+
+//     private int countWays(int[] nums, int index, int target, Map<String, Integer> memo) {
+//         String key = index + "," + target;
+
+//         if (memo.containsKey(key)) {
+//             return memo.get(key);
+//         }
+
+//         if (index == nums.length) {
+//             return (target == 0) ? 1 : 0;
+//         }
+
+//         int add = countWays(nums, index + 1, target - nums[index], memo);
+//         int subtract = countWays(nums, index + 1, target + nums[index], memo);
+
+//         memo.put(key, add + subtract);
+//         return add + subtract;
+//     }
+// }
+
+
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return countWays(nums, 0, target);
-    }
+        int n = nums.length;
+        int sum = 0;
 
-    private int countWays(int[] nums, int index, int target) {
-        
-        if (index == nums.length) {
-            return (target == 0) ? 1 : 0;
+        for (int num : nums) {
+            sum += num;
         }
 
         
-        int add = countWays(nums, index + 1, target - nums[index]);
+        if (target > sum || target < -sum) return 0;
 
-        
-        int subtract = countWays(nums, index + 1, target + nums[index]);
+        int[][] dp = new int[n + 1][2 * sum + 1];
+        dp[0][sum] = 1; 
 
-        return add + subtract;
+        for (int i = 1; i <= n; i++) {
+            int num = nums[i - 1];
+            for (int j = 0; j < 2 * sum + 1; j++) {
+                if (dp[i - 1][j] != 0) {
+                    int plus = j + num;
+                    int minus = j - num;
+
+                    if (plus < 2 * sum + 1)
+                        dp[i][plus] += dp[i - 1][j];
+                    if (minus >= 0)
+                        dp[i][minus] += dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[n][target + sum]; 
     }
 }
