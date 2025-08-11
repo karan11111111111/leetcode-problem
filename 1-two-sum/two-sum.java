a084333a788120cdeb4d7d005470801803823023
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-08-11 19:42:06 +0530

    Time: 2 ms (98.96%) | Memory: 45.2 MB (30.58%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,61 +1,40 @@
 // class Solution {
 //     public int[] twoSum(int[] nums, int target) {
-//        int n = nums.length;
 
-//        int b[] =new  int[2];
-//        for(int i=0; i<n; i++){
-//         for(int j=i+1; j<n; j++){
-//             if(nums[i]+nums[j]==target){
-//               b[0] = i;
-//               b[1] = j;  
-//               return b;
+//         HashMap<Integer, Integer> map = new HashMap<>();
+
+//         for(int i=0; i<nums.length; i++){
+//             int complement = target-nums[i];
+
+//             if(map.containsKey(complement)){
+
+//               return new int[]{map.get(complement),i};
 //             }
+
+//             map.put(nums[i],i);
 //         }
-//        }
 
-//     return null;
+//         return new int[]{};
+        
 //     }
 // }
-
-// class Solution {
-//     public int[] twoSum(int[] nums, int target) {
-//          for(int i = 1;i<nums.length;i++)
-//          {
-//             for(int j=i;j<nums.length;j++)
-//             {
-//                 if(nums[j]+nums[j-i]==target)
-//                 {
-//                     return new int[] {j,j-i};
-//                 }
-//             }
-//          }
-//          return null;
-//     }
-// }
-
-
-import java.util.HashMap;
-
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // Create a HashMap to store the number and its index
+
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        // Iterate through the array
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];  // Find the complement
+        for(int i=0; i<nums.length; i++){
+            int complement = target-nums[i];
 
-            // Check if the complement is already in the map
-            if (map.containsKey(complement)) {
-                // Return the indices of the complement and the current number
-                return new int[] { map.get(complement), i };
+            if(map.containsKey(complement)){
+
+              return new int[]{map.get(complement),i};
             }
 
-            // Add the current number and its index to the map
-            map.put(nums[i], i);
+            map.put(nums[i],i);
         }
 
-        // Return null if no solution is found
-        return null;
+        return new int[]{};
+        
     }
 }
