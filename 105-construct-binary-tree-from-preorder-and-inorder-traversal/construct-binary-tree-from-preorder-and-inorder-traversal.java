c90de40d91630b453fe80dba783722d97427fa62
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-09-10 17:35:44 +0530

    Time: 2 ms (60.25%) | Memory: 44.3 MB (77.72%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -13,30 +13,68 @@
  *     }
  * }
  */
+
+ //bruteforce
+ 
+// class Solution {
+
+//     private int preIndex = 0; // pointer for preorder array
+
+//     public TreeNode buildTree(int[] preorder, int[] inorder) {
+//         return build(preorder, inorder, 0, inorder.length - 1);
+//     }
+
+//     private TreeNode build(int[] preorder, int[] inorder, int inStart, int inEnd) {
+//         if (inStart > inEnd) return null;
+
+        
+//         int rootVal = preorder[preIndex++];
+//         TreeNode root = new TreeNode(rootVal);
+
+       
+//         int inIndex = inStart;
+//         while (inIndex <= inEnd && inorder[inIndex] != rootVal) {
+//             inIndex++;
+//         }
+
+       
+//         root.left = build(preorder, inorder, inStart, inIndex - 1);
+//         root.right = build(preorder, inorder, inIndex + 1, inEnd);
+
+//         return root;
+//     }
+// }
+
+
+import java.util.*;
+
 class Solution {
 
     private int preIndex = 0; // pointer for preorder array
+    private Map<Integer, Integer> inorderIndexMap = new HashMap<>();
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return build(preorder, inorder, 0, inorder.length - 1);
+        // Build a hashmap for quick lookup of inorder indices
+        for (int i = 0; i < inorder.length; i++) {
+            inorderIndexMap.put(inorder[i], i);
+        }
+
+        return build(preorder, 0, inorder.length - 1);
     }
 
-    private TreeNode build(int[] preorder, int[] inorder, int inStart, int inEnd) {
+    private TreeNode build(int[] preorder, int inStart, int inEnd) {
         if (inStart > inEnd) return null;
 
-        
+        // Current root from preorder
         int rootVal = preorder[preIndex++];
         TreeNode root = new TreeNode(rootVal);
 
-       
-        int inIndex = inStart;
-        while (inIndex <= inEnd && inorder[inIndex] != rootVal) {
-            inIndex++;
-        }
+        // Get root index in inorder from hashmap (O(1))
+        int inIndex = inorderIndexMap.get(rootVal);
 
-       
-        root.left = build(preorder, inorder, inStart, inIndex - 1);
-        root.right = build(preorder, inorder, inIndex + 1, inEnd);
+        // Build left and right subtrees
+        root.left = build(preorder, inStart, inIndex - 1);
+        root.right = build(preorder, inIndex + 1, inEnd);
 
         return root;
     }
