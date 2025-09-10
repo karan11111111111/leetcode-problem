72608fac05f554e13878d325b915ed966cdec6b8
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-09-10 16:38:40 +0530

    Time: 16 ms (22.21%) | Memory: 44.5 MB (55.32%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,23 +1,56 @@
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+
+//         if (s.length() != t.length()) {
+//             return false;
+//         }
+
+//         int n = s.length() ;
+//         char[] sarr = s.toCharArray();
+//         char[] tarr = t.toCharArray();
+
+//         Arrays.sort(tarr);
+//         Arrays.sort(sarr);
+//         for (int k=0; k < n; k++) {
+//             if (sarr[k]!=tarr[k]) {
+//                 return false;
+//             }
+//         }
+
+//         return true;
+
+//     }
+// }
+
+
+import java.util.*;
+
 class Solution {
     public boolean isAnagram(String s, String t) {
-
         if (s.length() != t.length()) {
             return false;
         }
 
-        int n = s.length() ;
-        char[] sarr = s.toCharArray();
-        char[] tarr = t.toCharArray();
+        Map<Character, Integer> map = new HashMap<>();
 
-        Arrays.sort(tarr);
-        Arrays.sort(sarr);
-        for (int k=0; k < n; k++) {
-            if (sarr[k]!=tarr[k]) {
-                return false;
+        // Count frequency for s
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        // Decrease frequency using t
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            if (!map.containsKey(ch)) {
+                return false; // character not found in s
+            }
+            map.put(ch, map.get(ch) - 1);
+            if (map.get(ch) == 0) {
+                map.remove(ch);
             }
         }
 
-        return true;
-
+        return map.isEmpty();
     }
 }
