1f8476edc405bfac86569c032deb1df062ec8256
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-11-26 10:11:26 +0530

    Time: 15 ms (52.55%) | Memory: 96.3 MB (8.33%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,30 @@
+class Solution {
+    // public boolean containsDuplicate(int[] nums) {
+        
+    //     for(int i=0; i<nums.length; i++){
+    //         for(int j=i+1; j<nums.length; j++){
+    //             if(nums[i] == nums[j]){
+    //                 return true;
+    //             }
+    //         }
+    //     }
+
+    //     return false;
+        
+    // }
+    public boolean containsDuplicate(int[] nums) {
+        
+      Map<Integer, Boolean> mp = new HashMap<>();
+
+      for(int num: nums){
+       
+       if(mp.containsKey(num)){
+        return true;
+       }
+       mp.put(num,true);
+
+      }
+        return false;
+        
+    }
+}
