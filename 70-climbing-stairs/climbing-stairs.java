56da57cfb43b8ce701affdc38e6c46a802c47e66
Karan <144606876+karan11111111111@users.noreply.github.com>
2026-01-16 22:23:45 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (8.64%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -7,26 +7,34 @@
 //         return  climbStairs(n-1)+ climbStairs(n-2);
 //     }
 // }
+
+class Solution {
+    int[] dp;
+
+    public int climbStairs(int n) {
+        dp = new int[n + 1];   // initialize once
+        return helper(n);
+    }
+
+    public int helper(int n) {
+        if (n == 0 || n == 1) return 1;
+
+        if (dp[n] != 0) return dp[n];
+
+        dp[n] = helper(n - 1) + helper(n - 2);
+        return dp[n];
+    }
+}
+
+
 // class Solution {
 //     public int climbStairs(int n) {
-//         if(n == 1 || n==0){
-//             return 1;
-//         }
 //         int dp[] = new int[n+1];
-
-//         dp[n] =  climbStairs(n-1)+ climbStairs(n-2);
+//         dp[0] =1;
+//         dp[1] =1;
+//         for(int i=2; i<=n; i++)
+//         dp[i] =  dp[i-1] + dp[i-2];
 
 //         return dp[n];
 //     }
 // }
-class Solution {
-    public int climbStairs(int n) {
-        int dp[] = new int[n+1];
-        dp[0] =1;
-        dp[1] =1;
-        for(int i=2; i<=n; i++)
-        dp[i] =  dp[i-1] + dp[i-2];
-
-        return dp[n];
-    }
-}
