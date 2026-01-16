a61187a7e8cbdfeb24546feaebcd0573210a956e
Karan <144606876+karan11111111111@users.noreply.github.com>
2026-01-16 22:12:20 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (94.46%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,73 +1,32 @@
+// class Solution {
+//     public int climbStairs(int n) {
+//         if(n == 1 || n==0){
+//             return 1;
+//         }
+
+//         return  climbStairs(n-1)+ climbStairs(n-2);
+//     }
+// }
+// class Solution {
+//     public int climbStairs(int n) {
+//         if(n == 1 || n==0){
+//             return 1;
+//         }
+//         int dp[] = new int[n+1];
+
+//         dp[n] =  climbStairs(n-1)+ climbStairs(n-2);
+
+//         return dp[n];
+//     }
+// }
 class Solution {
+    public int climbStairs(int n) {
+        int dp[] = new int[n+1];
+        dp[0] =1;
+        dp[1] =1;
+        for(int i=2; i<=n; i++)
+        dp[i] =  dp[i-1] + dp[i-2];
 
-    //recursion
-
-    // public int climbStairs(int n) {
-    //     if(n==0 || n==1){
-    //         return 1;
-    //     }
-        
-    //     return climbStairs(n-1) +climbStairs(n-2);
-    // }
-
-
-   //memoiozation
-
-    // public int climbStairs(int n) {
-    //     int[] memo = new int[n+1];
-    //     return helper(n,memo);
-        
-    // }
-
-    // private int helper(int n, int[]memo){
-    //     if(n==0 || n==1){
-    //         return 1;
-    //     }
-        
-    //     if(memo[n] !=0){
-    //         return memo[n];
-    //     }
-
-    //     memo[n] = helper(n-1,memo) + helper(n-2,memo);
-
-    //     return memo[n];
-    // }
-
-
-    // Tabulation
-
-    // public int climbStairs(int n) {
-
-        
-    //     int[] tabul = new int[n+1];
-    //     tabul[0] =1;
-    //     tabul[1] =1;
-
-    //     for(int i=2; i<=n; i++){
-    //         tabul[i] = tabul[i-1] + tabul[i-2];
-    //     }
-
-    //     return tabul[n];
-    // }
-
-    //space optimization
-
-
-  public int climbStairs(int n) {
-       
-   
-        if(n==0 || n==1){
-            return 1;
-        }
-       int first =1, second = 1;
-        for(int i=2; i<=n; i++){
-            int current = first + second;
-            first = second;
-            second = current;
-        }
-        
-       return second;
+        return dp[n];
     }
-
-   
 }
