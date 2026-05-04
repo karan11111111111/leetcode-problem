fa38e02e46c097a262303924996a82c5ebd06b1e
Karan <144606876+karan11111111111@users.noreply.github.com>
2026-05-04 19:48:27 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (51.87%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,32 +1,11 @@
-// class Solution {
-//     public void rotate(int[][] matrix) {
-//         int n = matrix.length;
-//         int[][] temp = new int[n][n];
-
-//         // Copy values to the rotated positions
-//         for (int i = 0; i < n; i++) {
-//             for (int j = 0; j < n; j++) {
-//                 temp[j][n - 1 - i] = matrix[i][j];
-//             }
-//         }
-
-//         // Copy temp back to original matrix
-//         for (int i = 0; i < n; i++) {
-//             for (int j = 0; j < n; j++) {
-//                 matrix[i][j] = temp[i][j];
-//             }
-//         }
-//     }
-// }
-
 class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
 
-        // Step 1: Transpose the matrix (swap matrix[i][j] with matrix[j][i])
+        // Step 1: Transpose the matrix
+        // We only iterate over the upper triangle (j > i) to avoid flipping twice
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                // Swap
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
@@ -35,17 +14,12 @@ class Solution {
 
         // Step 2: Reverse each row
         for (int i = 0; i < n; i++) {
-            int start = 0, end = n - 1;
-            while (start < end) {
-                // Swap
-                int temp = matrix[i][start];
-                matrix[i][start] = matrix[i][end];
-                matrix[i][end] = temp;
-
-                start++;
-                end--;
+            // Standard two-pointer swap for the row
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
             }
         }
     }
 }
-
